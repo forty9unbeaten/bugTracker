64846cf783e127594cfd8ce243c88a1a77a162e1
Rob Spears <rob.sp3ars@gmail.com>
2020-05-20 15:52:21 -0400

    Initial Django scaffolding

---
+++ bugTracker_tickets/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BugtrackerTicketsConfig(AppConfig):
+    name = 'bugTracker_tickets'
---
+++ bugTracker_users/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BugtrackerUsersConfig(AppConfig):
+    name = 'bugTracker_users'
