36a0770388fe50f9b8ed3f76617b86da8a6b4a08
Rob Spears <rob.sp3ars@gmail.com>
2020-05-21 09:05:21 -0400

    Add url paths for logging out and creating new users

--- bugTracker_users/urls.py
+++ bugTracker_users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from bugTracker_users import views
 
 url_paths = [
-    path('/login', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('users/new', views.new_user_view, name='new_user')
 ]
