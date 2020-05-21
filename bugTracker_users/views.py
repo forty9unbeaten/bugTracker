0f0dd1304fc5a0b26a1cadc12120f9733bbef529
Rob Spears <rob.sp3ars@gmail.com>
2020-05-21 07:05:52 -0400

    Implement GET request handling for login_view

--- bugTracker_users/views.py
+++ bugTracker_users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from bugTracker_users.forms import LoginForm
 
 # Create your views here.
+
+
+def login_view(request):
+    # GET request handling
+    form = LoginForm()
+    html = 'form.html'
+    return render(
+        request,
+        html,
+        {
+            'page_title': 'Log In',
+            'button_value': 'Log In',
+            'form': form
+        }
+    )
