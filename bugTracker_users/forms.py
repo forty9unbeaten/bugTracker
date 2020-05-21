381b94743ece28f2bb308782f5da6882e73de2e2
Rob Spears <rob.sp3ars@gmail.com>
2020-05-21 07:04:31 -0400

    Implement Login Form

--- bugTracker_users/forms.py
+++ bugTracker_users/forms.py
@@ -1,1 +1,6 @@
-from django.forms import Form, ModelForm
+import django.forms as forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(max_length=25, widget=forms.PasswordInput)
