bd267aaf7018b29a26cf71c9f4bb7708de63a9c6
Rob Spears <rob.sp3ars@gmail.com>
2020-05-21 09:03:58 -0400

    Create NewUser form

--- bugTracker_users/forms.py
+++ bugTracker_users/forms.py
@@ -4,3 +4,11 @@ import django.forms as forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(max_length=25, widget=forms.PasswordInput)
+
+
+class NewUserForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(max_length=25, widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
+        max_length=25, widget=forms.PasswordInput)
+    display_name = forms.CharField(max_length=50)
