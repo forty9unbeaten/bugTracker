3a8b198095fdc51ef7511a17a099741661ccdc72
Rob Spears <rob.sp3ars@gmail.com>
2020-05-21 12:41:35 -0400

    Add user detail url path

--- bugTracker_users/urls.py
+++ bugTracker_users/urls.py
@@ -4,5 +4,6 @@ from bugTracker_users import views
 url_paths = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('users/<int:userId>', views.user_detail_view),
     path('users/new', views.new_user_view, name='new_user')
 ]
